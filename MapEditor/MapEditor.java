837e6752aa18b7a25e4848bbf2a5a0cd0aa5e6a5
unknown <Zynine@.(none)>
2011-05-26 19:11:09 -0400

    Map Editor updated, began actual creating.

--- MapEditor/MapEditor.java
+++ MapEditor/MapEditor.java
@@ -1,25 +1,17 @@
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JTextArea;
-
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 public class MapEditor {
 	public static JButton Create;
 	public static JTextArea MapX;
-	public static int MapXi;
-	public static int MapYi;
 	public static JTextArea MapY;
 	public static JFrame NewBox;
 	private static Action action = new Action();
-	public static JButton[][] Pixels;
 	public static JButton New = new JButton("New");
 	public static JButton Load = new JButton("Load");
 	public static JButton Save = new JButton("Save");
 	static JFrame MapEdit = new JFrame("Map Editor");
+	static JButton[][] Pixels = new JButton[10][10];
 	public static void main(String[] args) {
 	GridLayout layout=new GridLayout(2,1);
 	MapEdit.setSize(200,200);
@@ -57,29 +49,52 @@ public class MapEditor {
 }
 }
 class Action implements ActionListener {
+	public static int MapXi;
+	public static int MapYi;
+	Color black;
+	@SuppressWarnings("deprecation")
 	public void actionPerformed(ActionEvent e) {
+		JButton id = (JButton) e.getSource();
 		if(e.getSource() == MapEditor.New)
 		{
 			MapEditor.New();
 		}
 		if(e.getSource() == MapEditor.Create)
 		{
-			MapEditor.MapXi = Integer.parseInt(MapEditor.MapX.getText());
-			MapEditor.MapYi = Integer.parseInt(MapEditor.MapY.getText());
-			MapEditor.MapEdit.setSize(Integer.parseInt(MapEditor.MapX.getText())*10,Integer.parseInt(MapEditor.MapY.getText())*10);
-			MapEditor.New.setVisible(false);
-			MapEditor.Load.setVisible(false);
+			JFrame Map = new JFrame("Map Editor");
+			MapXi = Integer.parseInt(MapEditor.MapX.getText());
+			MapYi = Integer.parseInt(MapEditor.MapY.getText());
+			Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+			if(screenSize.width <= Integer.parseInt(MapEditor.MapX.getText())* 10 || screenSize.height <= Integer.parseInt(MapEditor.MapY.getText())* 10)
+			{
+				Map.setBounds(0,0,screenSize.width, screenSize.height);
+				Map.setSize(screenSize.width, screenSize.height);
+			} else {
+				Map.setSize(Integer.parseInt(MapEditor.MapX.getText())*10,Integer.parseInt(MapEditor.MapY.getText())*10);
+				Map.setBounds(0,0,Integer.parseInt(MapEditor.MapX.getText())* 10, Integer.parseInt(MapEditor.MapY.getText())* 10);
+			}
+        	Map.setLayout(new GridLayout(Integer.parseInt(MapEditor.MapX.getText()),Integer.parseInt(MapEditor.MapY.getText())));
+        	MapEditor.Pixels=new JButton[Integer.parseInt(MapEditor.MapX.getText())][Integer.parseInt(MapEditor.MapY.getText())];
 			for(int i=0;i<Integer.parseInt(MapEditor.MapX.getText());i++) {
 				for(int j=0;j<Integer.parseInt(MapEditor.MapY.getText());j++) {
-					MapEditor.Pixels[i][j]= new JButton(" ");
-					MapEditor.Pixels[i][j].setSize(10,10);
+					MapEditor.Pixels[i][j]= new JButton();
+					MapEditor.Pixels[i][j].setLocation(Integer.parseInt(MapEditor.MapX.getText())*5,Integer.parseInt(MapEditor.MapY.getText())*5);
 					MapEditor.Pixels[i][j].addActionListener(this);
 					MapEditor.Pixels[i][j].setVisible(true);
-					MapEditor.MapEdit.setVisible(true);
-					MapEditor.MapEdit.add(MapEditor.Pixels[i][j]);
+					MapEditor.Pixels[i][j].setName(i + "-" + j);
+					Map.setVisible(true);
+					Map.add(MapEditor.Pixels[i][j]);
 				}
 			}
-			MapEditor.NewBox.setVisible(false);
+		}
+		if(id.getName() != null)
+		{
+		String[] MapId;
+		MapId = id.getName().split("-");
+		int MapX = Integer.parseInt(MapId[0]);
+		int MapY = Integer.parseInt(MapId[1]);
+		MapEditor.Pixels[MapX][MapY].setBackground(black);
+		System.out.println("Pixel "+MapX+","+MapY+" has been activated!");
 		}
 	}
 }
